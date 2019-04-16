b09feca8de0c78daa6a7d3fb7601cd08f4b336ec
Pradip Kandel <pradeepkandel008@gmail.com>
2019-04-16 21:57:28 +0545

    splash screen added and changed manifest.xml file

---
+++ app/src/main/java/com/example/collegeapp/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.collegeapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Intent intent=new Intent(SplashScreenActivity.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
